e49ca897d81dab8362fe6f7229af18ffc37e2cb0
Kerstorm <144790219+Kerstorm@users.noreply.github.com>
2023-11-20 21:17:17 +0700

    New

--- demo/src/main/java/JavaApi1/demo/Pack/model/Guild.java
+++ demo/src/main/java/JavaApi1/demo/Pack/model/Guild.java
@@ -1,14 +1,8 @@
 package JavaApi1.demo.Pack.model;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public record Guild(Integer id, String title, Date createDate, Integer countMembers, boolean pvp) {
-
-    public Guild(Integer id, String title, String createDate, Integer countMembers, boolean pvp) throws ParseException {
-        this(id, title, new SimpleDateFormat("dd.MM.yyyy").parse(createDate), countMembers, pvp);
-    }
+public record Guild(int id, String title, Date createDate, Integer countMembers, boolean pvp) {
 }
 
 
